f5420cc98aa84078292433cb0de54a6336a369e5
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-06 10:13:22 -0300

    Parallax effect added to onboarding view

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,6 +13,10 @@ struct OnboardingView: View {
   @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
   @State private var buttonOffset: CGFloat = 0
   @State private var isAnimating = false
+  @State private var imageOffset: CGSize = .zero //CGSize(width: 0, height: 0)
+  @State private var indicatorOpacity = 1.0
+  @State private var textTitle: String = "Share."
+  
   var body: some View {
     ZStack {
       Color("ColorBlue")
@@ -20,10 +24,14 @@ struct OnboardingView: View {
       VStack(spacing: 20) {
         Spacer()
         VStack(spacing: 0) {
-          Text("Share.")
+          Text(textTitle)
             .font(.system(size: 60))
             .fontWeight(.heavy)
             .foregroundColor(.white)
+            .transition(.opacity)
+            // Use id method here to tell SwiftUI that the View is no longer the same
+            // and then execute the transition method
+            .id(textTitle)
           
           Text("""
                 It's not how much we give but
@@ -41,12 +49,50 @@ struct OnboardingView: View {
         
         ZStack {
           CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+            .offset(x: imageOffset.width * -1)
+            .blur(radius: abs(imageOffset.width / 5))
+            .animation(.easeOut(duration: 1), value: imageOffset)
+          
           Image("character-1")
             .resizable()
             .scaledToFit()
             .opacity(isAnimating ? 1 : 0)
             .animation(.easeOut(duration: 0.5), value: isAnimating)
+            .offset(x: imageOffset.width * 1.2, y: 0)
+            .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+            .gesture(
+              DragGesture()
+                .onChanged { gesture in
+                  if abs(imageOffset.width) <= 150 {
+                    imageOffset = gesture.translation
+                    
+                    withAnimation(.linear(duration: 0.20)) {
+                      indicatorOpacity = 0
+                      textTitle = "Give."
+                    }
+                  }
+                }
+                .onEnded { _ in
+                  imageOffset = .zero
+                  
+                  withAnimation(.linear(duration: 0.20)) {
+                    indicatorOpacity = 1
+                    textTitle = "LOVE."
+                  }
+                }
+            ) //: GESTURE
+            .animation(.easeOut(duration: 1), value: imageOffset)
         } //: CENTER
+        .overlay(
+          Image(systemName: "arrow.left.and.right.circle")
+            .font(.system(size: 44, weight: .ultraLight))
+            .foregroundColor(.white)
+            .offset(y: 20)
+            .opacity(isAnimating ? 1 : 0)
+            .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+            .opacity(indicatorOpacity)
+          , alignment: .bottom
+        )
         
         ZStack {
           Capsule()
